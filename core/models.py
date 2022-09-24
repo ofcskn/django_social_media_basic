eaec331fe793bf61731ce486208b4fa53f5bf3a0
Omer Faruk Coskun <ofcskn.omer@gmail.com>
2022-09-24 19:24:55 +0300

    Post and user models are added

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=128)
+    username = models.CharField(max_length=64)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=16,blank=True)
+    password_hash = models.CharField(max_length=64)
+    image_name = models.CharField(max_length=256)
+    status = models.BooleanField(default=False)
+
+
+class Post(models.Model):
+    description = models.CharField(max_length=2056)
+    posted_user_id = models.ForeignKey("User", on_delete=models.CASCADE)
+    image_name = models.CharField(max_length=256)
+    created_date = models.DateTimeField(default= datetime.now)
+    
