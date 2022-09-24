248c00817835aa4c15c636b6ab3f53a07efaf238
Omer Faruk Coskun <ofcskn.omer@gmail.com>
2022-09-24 23:14:11 +0300

    Login functions are updated to View

--- account/views.py
+++ account/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class LoginView(View):
+    template_name = 'account/login.html'
+    def get(self, request):
+        print('get_called')
+        return render(request, self.template_name)
+    def post(self, request):
+        print('post_called')
+        pass
+
 def register(request):
     return render(request, 'account/register.html')
-
-def login(request):
-    return render(request, 'account/login.html')
-
-def login_post(request):
-    return render(request, 'account/login.html')
 
 def profile(request):
     return render(request, 'account/profile.html')
