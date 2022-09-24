0e1ec5f26557e19bb787763f76565f429e689e3f
Omer Faruk Coskun <ofcskn.omer@gmail.com>
2022-09-24 23:55:49 +0300

    Login is completed #1

--- account/forms.py
+++ account/forms.py
@@ -2,7 +2,13 @@ from django.forms import ModelForm
 from django import forms
 from .models import  User
 
-class UserForm(ModelForm):
+class UserLoginForm(ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ['username']
+
+class UserRegisterForm(ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
--- account/views.py
+++ account/views.py
@@ -1,20 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.http import HttpResponseRedirect
-from .forms import UserForm
+from django.http import HttpResponseRedirect, HttpResponse
+
+import hashlib, uuid
+
+from account.models import User
+from .forms import UserLoginForm
+
+def createHashedPassword(email, username, password):
+    # create salt for passwords
+    salt = uuid.uuid4().hex + email + uuid.uuid4().hex + username + uuid.uuid4().hex
+    hashed_password = hashlib.sha512(password + salt).hexdigest()
+    return hashed_password
 
 # Create your views here.
 class LoginView(View):
+    form_class = UserLoginForm
     template_name = 'account/login.html'
     def get(self, request):
         return render(request, self.template_name)
     def post(self, request):
-        print(self)
         form = self.form_class(request.POST)
         if form.is_valid():
-            print("success", form)
-            # <process form cleaned data>
-            return HttpResponseRedirect('/')
+            userName = form.cleaned_data['username']
+            passwordEntered = form.cleaned_data['password']
+
+            # get user from the database
+            user = get_object_or_404(User,username=userName)
+            hashedPassword = createHashedPassword(user.email, user.username, passwordEntered)
+            if hashedPassword == user.password_hash:          
+                # add user login log to the database (task)
+                return HttpResponseRedirect("/")
+            else:
+                return HttpResponse("password-wrong")
 
         return render(request, self.template_name, {'form': form})
 
