7a227b7debcbbdfa3b2367191ba4097688057867
Omer Faruk Coskun <ofcskn.omer@gmail.com>
2022-09-24 18:14:52 +0300

    Some views and urls are added

---
+++ core/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # home page
+    path('', views.home, name="home"),
+    # posts from all users as random
+    path('explore', views.explore, name="explore"),
+    # profile of the user
+    path('<str:user_name>', views.get_user, name="get_user"),
+    # followers of the user
+    path('<str:user_name>/followers', views.get_followers_of_user, name="get_followers_of_user"),
+    # saved posts of the user
+    path('<str:user_name>/posts/saved', views.saved_posts_of_user, name="saved_posts_of_user"),
+    # liked posts of the user
+    path('<str:user_name>/posts/liked', views.liked_posts_of_user, name="liked_posts_of_user"),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
 # Create your views here.
+def home(request):
+    return HttpResponse("this is infinity home/posts page")
+
+def explore(request):
+    return HttpResponse("this is explore page")
+
+def get_user(request, user_name):
+    return HttpResponse(user_name)
+    
+def get_followers_of_user(request, user_name):
+    return HttpResponse("Followers of " + user_name)
+        
+def saved_posts_of_user(request, user_name):
+    return HttpResponse("Saved posts of " + user_name)
+
+def liked_posts_of_user(request, user_name):
+    return HttpResponse("Liked posts of " + user_name)
